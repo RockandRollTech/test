d1f98cb0fe3c679e7d501e88b91d900f5433d048
RockandRollTech <134345779+RockandRollTech@users.noreply.github.com>
2023-06-11 23:28:58 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,62 @@
 <?php
 
-switch ($_GET['ui']) {
-    case 'kiosk-home':
-        echo "UI kiosk home";
+$html-header = '
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport"
+         content="width=device-width,
+                        initial-scale=1.0" />
+    <title>Check In or Check Out</title>
     
+    <style>
+        h1, h3 {
+          color: green;
+        }
+        body, header {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+    </style>
+    <script src=
+"https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js">
+    </script>
+</head><body><center>';
 
+$html-footer = '</center></body></html>';
+
+switch ($_POST['ui']) {
+    default: //Kiosk Home - show QR code + button for checkin/checkout without mobile + button for "I don't have reception here"
+        echo $html-header;
+        echo "UI kiosk home<br>";
+        echo '<main>
+              <div id="qrcode"></div>
+              </main>
+     
+                <script>
+                    var qrcode = new QRCode("qrcode",
+                    "https://www.geeksforgeeks.org");
+                </script>';
+        echo $html-footer;
     
         break;
+        
+    case 'kiosk-form':
+        echo "UI kiosk checkin or out";
+        //show form for check in or out, 2 buttons (in/out)
     
+        break;
+        
+    case 'kiosk-wifi-QR':
+        echo "UI kiosk checkout";
+        //show QR code for kiosk WiFi
+    
+        break;
+        
     case 'mobile-home':
         echo "UI mobile home";
         $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
