e1fffbc3a7ffe50416b7a8d09b9e40297d7ed7fc
RockandRollTech <134345779+RockandRollTech@users.noreply.github.com>
2023-06-12 00:45:24 +1000

    Create vars.php

---
+++ vars.php
@@ -0,0 +1,28 @@
+$htmlheader = '
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport"
+         content="width=device-width,
+                        initial-scale=1.0" />
+    <title>Check In or Check Out</title>
+    
+    <style>
+        h1, h3 {
+          color: green;
+        }
+        body, header {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+    </style>
+    <script src=
+"https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js">
+    </script>
+</head><body><center>';
+
+$htmlfooter = '</center></body></html>';
