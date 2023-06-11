0fc2ec80ea03e8d68a8e31f7264618c4fe2ed154
RockandRollTech <134345779+RockandRollTech@users.noreply.github.com>
2023-06-11 17:23:01 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 
-echo "test minimum basic.";
+switch ($_GET['ui']) {
+    case 'kiosk-home':
+        echo "UI kiosk home";
+    
+
+    
+        break;
+    
+    case 'mobile-home':
+        echo "UI mobile home";
+        $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+        $txt = "Donald Duck\n";
+        fwrite($myfile, $txt);
+        $txt = "Goofy Goof\n";
+        fwrite($myfile, $txt);
+        fclose($myfile); 
+        break;
+    
+    case 'report-home':
+        echo "UI report home";
+
+    break;
+}
+
+
 
