761835c6e3f7dd59a8524ca48eba7326f87069c8
RockandRollTech <134345779+RockandRollTech@users.noreply.github.com>
2023-06-12 00:44:58 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,6 @@
 <?php
 
-$htmlheader = '
-<!DOCTYPE html>
-<html lang="en">
- 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport"
-         content="width=device-width,
-                        initial-scale=1.0" />
-    <title>Check In or Check Out</title>
-    
-    <style>
-        h1, h3 {
-          color: green;
-        }
-        body, header {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-    </style>
-    <script src=
-"https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js">
-    </script>
-</head><body><center>';
-
-$htmlfooter = '</center></body></html>';
+include 'vars.php';
 
 switch ($_POST['ui']) {
     default: //Kiosk Home - show QR code + button for checkin/checkout without mobile + button for "I don't have reception here"
